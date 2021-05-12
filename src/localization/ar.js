4d7fed47f139f25eea2a7bc56a13d3b46d715ec1
Adam Albarghouthi <adambargh@gmail.com>
2021-05-12 18:46:48 -0400

    rm

--- src/localization/ar.js
+++
@@ -1,7 +0,0 @@
-const ar = {
-  title: "مرحباً بكم في",
-  subtitle: "ابدأ بتعديل",
-  poweredby: "بدعمٍ من",
-};
-
-export default ar;
--- src/localization/en.js
+++
@@ -1,7 +0,0 @@
-const en = {
-  title: "Welcome to",
-  subtitle: "Get started by editing",
-  poweredby: "Powered by",
-};
-
-export default en;
