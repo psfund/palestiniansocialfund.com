846cd93aae495f6bec0ec2c6d5f293afcf8392f9
Adam Albarghouthi <adambargh@gmail.com>
2021-05-12 18:41:25 -0400

    add next-i18next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
+const { i18n } = require("./next-i18next.config");
+
 module.exports = {
-  i18n: {
-    // These are all the locales you want to support in
-    // your application
-    locales: ["en", "ar"],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: "en",
-  },
+  i18n,
 };
