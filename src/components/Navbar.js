f4081cd3a0408d03dc53111c54c64bba64a9ac62
Adam Albarghouthi <adambargh@gmail.com>
2021-05-12 22:46:32 -0400

    add LocaleSwitch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import LocaleSwitch from "src/components/LocaleSwitch";
 import { BellIcon, MenuAlt2Icon } from "@heroicons/react/outline";
 import { SearchIcon } from "@heroicons/react/solid";
 
@@ -15,40 +16,8 @@ export default function Navbar(props) {
           <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
         </button>
         <div className="flex-1 flex justify-between px-4 md:px-0">
-          <div className="flex-1 flex">
-            <form className="w-full flex md:ml-0" action="#" method="GET">
-              <label htmlFor="search_field" className="sr-only">
-                Search
-              </label>
-              <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
-                  <SearchIcon
-                    className="flex-shrink-0 h-5 w-5"
-                    aria-hidden="true"
-                  />
-                </div>
-                <input
-                  name="search_field"
-                  id="search_field"
-                  className="h-full w-full border-transparent py-2 pl-8 pr-3 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-0 focus:border-transparent focus:placeholder-gray-400 sm:hidden"
-                  placeholder="Search"
-                  type="search"
-                />
-                <input
-                  name="search_field"
-                  id="search_field"
-                  className="hidden h-full w-full border-transparent py-2 pl-8 pr-3 text-sm text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-0 focus:border-transparent focus:placeholder-gray-400 sm:block"
-                  placeholder="Search"
-                  type="search"
-                />
-              </div>
-            </form>
-          </div>
-          <div className="ml-4 flex items-center md:ml-6">
-            <button className="bg-white rounded-full p-1 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
-              <BellIcon className="h-6 w-6" aria-hidden="true" />
-              <span className="sr-only">View notifications</span>
-            </button>
+          <div className="ms-auto flex items-center">
+            <LocaleSwitch />
           </div>
         </div>
       </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Container from "./Container";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
+import LocaleSwitch from "./LocaleSwitch";
 
-export { Container, Sidebar, Navbar };
+export { Container, Sidebar, Navbar, LocaleSwitch };
