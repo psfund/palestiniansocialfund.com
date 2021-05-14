abf46f8500fd62f1b4eee05f8d0b57566a212b57
Adam Albarghouthi <adambargh@gmail.com>
2021-05-14 12:30:53 -0400

    minor

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,9 +2,7 @@ export default function Container(props) {
   return (
     <div className="max-w-screen-xl mx-auto lg:ps-8">
       <div className="w-full mx-auto">
-        <div className="h-screen bg-white overflow-hidden flex">
-          {props.children}
-        </div>
+        <div className="h-screen bg-white flex">{props.children}</div>
       </div>
     </div>
   );
