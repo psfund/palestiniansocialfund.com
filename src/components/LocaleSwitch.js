c3c3c0e0f702cd3d69e03c69559b46070c75d4fd
Adam Albarghouthi <adambargh@gmail.com>
2021-06-10 16:55:43 -0400

    minor

--- src/components/LocaleSwitch.js
+++ src/components/LocaleSwitch.js
@@ -42,7 +42,7 @@ export default function LocaleSwitch() {
       <select
         id="locale"
         name="locale"
-        className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+        className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
         value={selected}
         onChange={(e) => setSelected(e.target.value)}
       >
