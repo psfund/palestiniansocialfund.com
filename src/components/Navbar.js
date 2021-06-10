7d1c5b20d1e6dba1033711dd81c4d398c694227c
Adam Albarghouthi <adambargh@gmail.com>
2021-06-10 17:17:34 -0400

    rm support fund button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,8 @@
 import LocaleSwitch from "src/components/LocaleSwitch";
 import { MenuAlt2Icon } from "@heroicons/react/outline";
-import Link from "next/link";
-import { useTranslation } from "next-i18next";
 
 export default function Navbar(props) {
   const { setSidebarOpen } = props;
-  const { t } = useTranslation();
 
   return (
     <div className="w-full mx-auto md:px-8">
@@ -20,11 +17,6 @@ export default function Navbar(props) {
         <div className="flex-1 flex justify-between px-4 md:px-0">
           <div className="ms-auto flex items-center space-s-2">
             <LocaleSwitch />
-            <Link href="/support">
-              <a className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-                {t("common:support_fund")}
-              </a>
-            </Link>
           </div>
         </div>
       </div>
