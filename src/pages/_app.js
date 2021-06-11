72fcb8e49a64f9a62fd2a3f9f4ae8040b40e5214
Adam Albarghouthi <adambargh@gmail.com>
2021-06-10 21:33:19 -0400

    rm lightgallery css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,6 @@
 import { appWithTranslation } from "next-i18next";
 import "src/styles/globals.css";
 
-import "lightgallery/css/lightgallery.css";
-import "lightgallery/css/lg-zoom.css";
-import "lightgallery/css/lg-thumbnail.css";
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
