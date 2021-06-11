5708e13321568aeed5290fe3e45837223316c2a1
Adam Albarghouthi <adambargh@gmail.com>
2021-06-10 21:28:29 -0400

    rm lightgallery, add iframe on coop

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,11 @@
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import dynamic from "next/dynamic";
 import { Line } from "rc-progress";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { MainLayout } from "src/layouts";
 import { stripe } from "src/clients";
 import Plans from "src/components/Plans";
-
-import lgThumbnail from "lightgallery/plugins/thumbnail";
-import lgZoom from "lightgallery/plugins/zoom";
-
-const LightGallery = dynamic(() => import("lightgallery/react"), {
-  ssr: false,
-});
 
 const people = [
   {
@@ -99,58 +91,9 @@ const Home = ({ balance }) => {
             </div>
           </dl>
         </div>
-        <LightGallery
-          speed={500}
-          elementClassNames="grid grid-flow-row grid-cols-2 md:grid-cols-4 grid-rows-1 gap-4 mb-5"
-          plugins={[lgThumbnail, lgZoom]}
-        >
-          <a
-            className="gallery-item"
-            data-src="https://scontent-bos3-1.xx.fbcdn.net/v/t1.6435-9/149348863_548031882797871_6102313069924439608_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=Ki9VQQQMr3AAX9VFwzN&_nc_ht=scontent-bos3-1.xx&oh=cbc0eafa524b4de6d5c97fe508256088&oe=60DEE60A"
-            data-sub-html="<h4>Photo by Kanaan's Land</h4><p> The chicken coop building that was constructed by the founders themselves.</p>"
-          >
-            <img
-              className="img-responsive"
-              src="https://scontent-bos3-1.xx.fbcdn.net/v/t1.6435-9/149348863_548031882797871_6102313069924439608_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=Ki9VQQQMr3AAX9VFwzN&_nc_ht=scontent-bos3-1.xx&oh=cbc0eafa524b4de6d5c97fe508256088&oe=60DEE60A"
-            />
-          </a>
-
-          <a
-            className="gallery-item"
-            data-src="https://scontent-bos3-1.xx.fbcdn.net/v/t1.6435-9/163424370_579638806303845_3203778093767521275_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=79nt2JWU3TcAX-ZDQph&_nc_ht=scontent-bos3-1.xx&oh=d470789c4e8f059145719b7ca9004ab6&oe=60DB90C3"
-            data-sub-html="<h4>Photo by Kanaan's Land</h4><p>The chickens enjoy the outdoors in the surrounding fields around the coop.</p>"
-          >
-            <img
-              className="img-responsive"
-              src="https://scontent-bos3-1.xx.fbcdn.net/v/t1.6435-9/163424370_579638806303845_3203778093767521275_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=79nt2JWU3TcAX-ZDQph&_nc_ht=scontent-bos3-1.xx&oh=d470789c4e8f059145719b7ca9004ab6&oe=60DB90C3"
-            />
-          </a>
-
-          <a
-            className="gallery-item"
-            data-src="https://scontent-bos3-1.xx.fbcdn.net/v/t1.6435-9/152495406_556678915266501_1967096318194036235_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=krRsSnxQDg4AX8zqWQ9&_nc_ht=scontent-bos3-1.xx&oh=0eac719b4cd7438f1db37d849ff28359&oe=60DBC94B"
-            data-sub-html="<h4>Photo by Kanaan's Land</h4><p>The chickens enjoy the outdoors in the surrounding fields around the coop.</p>"
-          >
-            <img
-              className="img-responsive"
-              src="https://scontent-bos3-1.xx.fbcdn.net/v/t1.6435-9/152495406_556678915266501_1967096318194036235_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=krRsSnxQDg4AX8zqWQ9&_nc_ht=scontent-bos3-1.xx&oh=0eac719b4cd7438f1db37d849ff28359&oe=60DBC94B"
-            />
-          </a>
-
-          <a
-            className="gallery-item"
-            data-src="https://scontent.fymq3-1.fna.fbcdn.net/v/t1.6435-9/163733696_579638736303852_8962526549451915618_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=rA3S9oj8dagAX_T4Ql5&_nc_ht=scontent.fymq3-1.fna&oh=4b180a485647ba57bce75460066802dc&oe=60E323D7"
-            data-sub-html="<h4>Photo by Kanaan's Land</h4><p>The chickens enjoy the outdoors in the surrounding fields around the coop.</p>"
-          >
-            <img
-              className="img-responsive"
-              src="https://scontent.fymq3-1.fna.fbcdn.net/v/t1.6435-9/163733696_579638736303852_8962526549451915618_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=rA3S9oj8dagAX_T4Ql5&_nc_ht=scontent.fymq3-1.fna&oh=4b180a485647ba57bce75460066802dc&oe=60E323D7"
-            />
-          </a>
-        </LightGallery>
         <iframe
           src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fardkanan007%2Fposts%2F561088291492230&width=500&show_text=false&appId=1701313576784649&height=582"
-          width="500"
+          width="100%"
           height="582"
           style={{ border: "none", overflow: "hidden" }}
           scrolling="no"
@@ -158,16 +101,6 @@ const Home = ({ balance }) => {
           allowfullscreen="true"
           allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
         ></iframe>
-        <div className="mt-5">
-          <Link href="https://www.facebook.com/ardkanan007/">
-            <a
-              target="_blank"
-              className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-[#4267B2] hover:bg-[#375695] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            >
-              {t("home:visit_fb")}
-            </a>
-          </Link>
-        </div>
       </div>
 
       <div className="mx-auto py-12 max-w-7xl">
