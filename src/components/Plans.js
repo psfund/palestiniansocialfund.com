67809422dbed5827b45a600e197880a61bc2c29e
Adam Albarghouthi <adambargh@gmail.com>
2022-04-20 19:34:56 -0400

    add new billing products

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,187 +1,120 @@
 import { useState } from "react";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
+import { AcademicCapIcon } from "@heroicons/react/outline";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const tiers = [
   {
-    name: "stone",
-    priceMonthly: 11,
-    hrefMonhtly: "https://buy.stripe.com/4gw4kc9s4fPQ796dQR",
-    priceYearly: 104,
-    hrefYearly: "https://buy.stripe.com/4gw180cEgfPQdxudQT",
+    name: "student",
+    priceMonthly: 6,
+    hrefMonhtly: "https://buy.stripe.com/fZe4kc1ZCcDE3WU008",
+    priceYearly: 65,
+    hrefYearly: "https://buy.stripe.com/dR6g2U6fSbzA796fZ5",
   },
   {
-    name: "slingshot",
-    priceMonthly: 29,
-    hrefMonhtly: "https://buy.stripe.com/3cs03W0Vy47850YeUZ",
-    priceYearly: 310,
-    hrefYearly: "https://buy.stripe.com/cN2dUM8o0dHI7969AG",
+    name: "seed",
+    priceMonthly: 15,
+    hrefMonhtly: "https://buy.stripe.com/8wM5oggUwcDE2SQbIS",
+    priceYearly: 150,
+    hrefYearly: "https://buy.stripe.com/28og2UcEgavwdxu9AJ",
+  },
+  {
+    name: "tree",
+    priceMonthly: 35,
+    hrefMonhtly: "https://buy.stripe.com/14k03W8o09rs0KI28k",
+    priceYearly: 400,
+    hrefYearly: "https://buy.stripe.com/aEU3g8cEg0UW9hedR1",
   },
 ];
 
-export default function Plans(props) {
-  const { t } = useTranslation();
-  const [billing, setBilling] = useState("yearly");
-  const { locale } = useRouter();
+export default function Plans() {
+  const [billing, setBilling] = useState("monthly");
 
   return (
-    <>
-      <div className="bg-white border rounded py-4 px-3">
-        <div className="max-w-7xl mx-auto bg-white">
-          {/* xs to lg */}
-          <div className="max-w-2xl mx-auto space-y-8 lg:hidden">
-            <div>
-              <label
-                htmlFor="billing"
-                className="block text-sm font-medium text-gray-700"
-              >
-                {t("common:billing_cycle")}
-              </label>
-              <select
-                id="billing"
-                name="billing"
-                className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                value={billing}
-                onChange={(e) => setBilling(e.target.value)}
-              >
-                <option value="monthly">{t("common:monthly")}</option>
-                <option value="yearly">{t("common:yearly")}</option>
-              </select>
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:flex-col sm:align-center">
+          <h1 className="text-5xl font-extrabold text-gray-900 sm:text-center">
+            Support the fund
+          </h1>
+          <p className="mt-5 text-xl text-gray-500 sm:text-center">
+            Your support, big or small, matters.
+          </p>
+          <div className="relative self-center mt-6 bg-gray-100 rounded-lg p-0.5 flex sm:mt-8">
+            <button
+              type="button"
+              className={classNames(
+                billing === "monthly"
+                  ? "bg-white border-gray-200 shadow-sm text-gray-900"
+                  : "border border-transparent text-gray-700",
+                "relative w-1/2 rounded-md py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-gray-500 focus:z-10 sm:w-auto sm:px-8"
+              )}
+              onClick={() => setBilling("monthly")}
+            >
+              Monthly support
+            </button>
+            <button
+              type="button"
+              className={classNames(
+                billing === "yearly"
+                  ? "bg-white border-gray-200 shadow-sm text-gray-900"
+                  : "border border-transparent text-gray-700",
+                "ml-0.5 relative w-1/2 rounded-md py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-gray-500 focus:z-10 sm:w-auto sm:px-8"
+              )}
+              onClick={() => setBilling("yearly")}
+            >
+              Yearly support
+            </button>
+          </div>
+        </div>
+        <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-3">
+          {tiers.map((tier) => (
+            <div
+              key={tier.name}
+              className={classNames(
+                tier.name === "student"
+                  ? "bg-gray-50"
+                  : "bg-white border border-gray-200",
+                "rounded-lg shadow-sm divide-y divide-gray-200"
+              )}
+            >
+              <div className="p-6">
+                <h2 className="flex items-center text-lg font-bold capitalize leading-6 font-medium text-gray-900">
+                  {tier.name === "student" && (
+                    <AcademicCapIcon className="h-8 w-8 mr-1" />
+                  )}
+                  {tier.name}
+                </h2>
+                <p className="mt-4 text-sm text-gray-500">{tier.description}</p>
+                <p className="mt-8">
+                  <span className="text-4xl font-extrabold text-gray-900">
+                    $
+                    {billing === "monthly"
+                      ? tier.priceMonthly
+                      : tier.priceYearly}{" "}
+                    USD
+                  </span>{" "}
+                  <span className="text-base font-medium text-gray-500">
+                    {billing}
+                  </span>
+                </p>
+                <a
+                  href={
+                    billing === "monthly" ? tier.hrefMonhtly : tier.hrefYearly
+                  }
+                  className="mt-8 block w-full bg-gray-800 border border-gray-800 rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-gray-900"
+                >
+                  Support
+                </a>
+              </div>
             </div>
-            {tiers.map((tier) => (
-              <section key={tier.name}>
-                <div className="p-4 border rounded mb-8">
-                  <h2 className="text-lg leading-6 font-medium text-gray-900">
-                    {t(`common:${tier.name}`)}
-                  </h2>
-                  <p className="mt-4">
-                    <span className="text-4xl font-extrabold text-gray-900">
-                      $
-                      {billing === "monthly"
-                        ? tier.priceMonthly
-                        : tier.priceYearly}
-                    </span>{" "}
-                    <span className="text-base font-medium text-gray-500">
-                      {billing === "monthly"
-                        ? t("common:monthly")
-                        : t("common:yearly")}
-                    </span>
-                  </p>
-                  <a
-                    href={
-                      billing === "monthly" ? tier.hrefMonhtly : tier.hrefYearly
-                    }
-                    className="mt-6 block border border-gray-800 rounded-md bg-gray-800 w-full py-2 text-sm font-semibold text-white text-center hover:bg-gray-900"
-                  >
-                    {t("common:choose")}
-                  </a>
-                </div>
-              </section>
-            ))}
-          </div>
-
-          {/* lg+ */}
-          <div className="hidden lg:block">
-            <table className="w-full h-px table-fixed">
-              <thead>
-                <tr>
-                  <th
-                    className={`pb-4 pe-6 text-sm font-medium text-gray-900 ${
-                      locale === "ar" ? "text-right" : "text-left"
-                    }`}
-                    scope="col"
-                  >
-                    <span> {t("common:billing_cycle")}</span>
-                  </th>
-                  {tiers.map((tier) => (
-                    <th
-                      key={tier.name}
-                      className={`w-1/3 pb-4 px-6 text-lg leading-6 font-medium text-gray-900 ${
-                        locale === "ar" ? "text-right" : "text-left"
-                      }`}
-                      scope="col"
-                    >
-                      {t(`common:${tier.name}`)}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody className="border-t border-gray-200 divide-y divide-gray-200">
-                <tr>
-                  <th
-                    className="py-8 pe-6 text-sm font-medium text-gray-900 text-left align-top"
-                    scope="row"
-                  >
-                    <div>
-                      <label
-                        htmlFor="billing"
-                        className="block text-sm font-medium text-gray-700 hidden"
-                      >
-                        Billing
-                      </label>
-                      <select
-                        id="billing"
-                        name="billing"
-                        className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                        value={billing}
-                        onChange={(e) => setBilling(e.target.value)}
-                      >
-                        <option value="monthly">{t("common:monthly")}</option>
-                        <option value="yearly">{t("common:yearly")}</option>
-                      </select>
-                    </div>
-                  </th>
-                  {tiers.map((tier) => (
-                    <td key={tier.name} className="h-full py-8 px-6 align-top">
-                      <div className="relative h-full w-full table">
-                        <p className="mb-6">
-                          <span className="text-4xl font-extrabold text-gray-900">
-                            $
-                            {billing === "monthly"
-                              ? tier.priceMonthly
-                              : tier.priceYearly}
-                          </span>{" "}
-                          <span className="text-base font-medium text-gray-500">
-                            {billing === "monthly"
-                              ? t("common:monthly")
-                              : t("common:yearly")}
-                          </span>
-                        </p>
-                        <a
-                          href={
-                            billing === "monthly"
-                              ? tier.hrefMonhtly
-                              : tier.hrefYearly
-                          }
-                          className="flex-grow block w-full bg-gray-800 border border-gray-800 rounded-md 5 py-2 text-sm font-semibold text-white text-center hover:bg-gray-900"
-                        >
-                          {t("common:choose")}
-                        </a>
-                      </div>
-                    </td>
-                  ))}
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-
-        <div className="rounded bg-gray-100 text-sm text-gray-500 p-3">
-          {t("home:one_time_disclaimer")}{" "}
-          <a
-            className="underline hover:text-gray-900"
-            href="https://buy.stripe.com/3cseYQcEg4783WU7ss"
-          >
-            {t("common:click_here")}
-          </a>
+          ))}
         </div>
       </div>
-
-      <div className="mt-1">
-        <p className="text-sm text-gray-400">
-          {t("home:processing_disclaimer")}
-        </p>
-      </div>
-    </>
+    </div>
   );
 }
