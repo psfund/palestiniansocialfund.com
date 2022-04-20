5a32235aee37c1f16eb92c8d8838da1795089d31
Adam Albarghouthi <adambargh@gmail.com>
2022-04-20 17:03:44 -0400

    add padding and fix footer

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -6,8 +6,8 @@ export default function MainLayout(props) {
       <div className="flex-1 flex flex-col">
         <Navbar />
         <div className="min-w-0 w-full flex-auto lg:static lg:max-h-full lg:overflow-visible">
-          <div className="mx-8 py-8">{props.children}</div>
-          <div className="border-t px-8 py-5">
+          <div className="mx-8 pt-8 pb-20">{props.children}</div>
+          <div className="fixed bottom-0 left-0 bg-white w-full border-t px-8 py-5">
             <div className="flex justify-between items-center">
               <p className="text-sm text-gray-500">
                 Palestinian Social Fund © Non-profit registered in Canada
