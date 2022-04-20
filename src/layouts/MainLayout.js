26d2a520a972c98808a0c79fb0d7efe8ea0f0bb0
Adam Albarghouthi <adambargh@gmail.com>
2022-04-20 19:35:06 -0400

    remove fixed footer

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -7,7 +7,7 @@ export default function MainLayout(props) {
         <Navbar />
         <div className="min-w-0 w-full flex-auto lg:static lg:max-h-full lg:overflow-visible">
           <div className="mx-8 pt-8 pb-20">{props.children}</div>
-          <div className="fixed bottom-0 left-0 bg-white w-full border-t px-8 py-5">
+          <div className="bg-white w-full border-t px-8 py-5">
             <div className="flex justify-between items-center">
               <p className="text-sm text-gray-500">
                 Palestinian Social Fund © Non-profit registered in Canada
