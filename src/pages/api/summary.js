7a1bd7aea7eaadd28598b2a1e00601090ba7b4ce
Adam Albarghouthi <adambargh@gmail.com>
2022-05-30 15:21:15 -0400

    initial commit

---
+++ src/pages/api/summary.js
@@ -0,0 +1,37 @@
+import { stripe } from "src/clients";
+
+async function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      const balance = await stripe.balance.retrieve();
+
+      const charges = await stripe.charges.search({
+        query: "status:'succeeded'",
+      });
+
+      const cleanCharges = charges.data.map((c) => ({
+        amount: c.amount,
+        status: c.paid ? "paid" : "",
+        date: c.created,
+        currency: c.currency,
+      }));
+
+      const subscriptions = await stripe.subscriptions.search({
+        query: "status:'active'",
+        limit: 100,
+      });
+
+      res.status(200).json({
+        payments: cleanCharges,
+        supporters: subscriptions.data.length,
+        balance: `${
+          (balance.available[0].amount + balance.pending[0].amount) / 100
+        } ${balance.available[0].currency.toUpperCase()}`,
+      });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+}
+
+export default handler;
