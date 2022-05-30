95063f1e09b1459126bcfceaf195cd844ea0e0c8
Adam Albarghouthi <adambargh@gmail.com>
2022-05-30 15:28:25 -0400

    make json file js file

---
+++ src/utils/posts.js
@@ -0,0 +1,71 @@
+const posts = [
+  {
+    place: "",
+    location: "Auja",
+    caption:
+      "To boycott the products and melons of the occupation, which is currently flooding our markets, let's support our Palestinian valleys and our steadfast farmers who are the protectors of the land.\nPalestinian watermelon from Auja / Palestinian Valley\nIt is available in the cooperative’s account, on Tuesday, 10-5, in front of the Folk Art Center (Al-Bireh, opposite Al-Ain Mosque), starting at 12 noon.",
+    images: [
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/279965504_1581039658947967_3759022604806509296_n.jpg?stp=dst-jpg_p720x720&_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=tJDcS7v5w5AAX_rMyjT&tn=0nVerItEGiwdW2Iv&_nc_ht=scontent-bos5-1.xx&oh=00_AT_LxzJ6YqH2ixgqYT9g63CDNFbb-f5X3P-3QndOu1soxA&oe=629998DA",
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/280080653_1581039958947937_2078416217724152718_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=RPHXokaRYvwAX__utrQ&_nc_ht=scontent-bos5-1.xx&oh=00_AT9ByborRRNzdCAOJigm22nhkxQ57VmWJWFlEAbg_g7u3A&oe=62994305",
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/279757827_1581039775614622_4533025287685629697_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=Ol3VByRpu0UAX-KFB8N&_nc_ht=scontent-bos5-1.xx&oh=00_AT8OKiLm3el_ZohhhU5m58hZGmtP93bMW4yvmGkYo9dkqA&oe=6299E364",
+    ],
+    url: "https://www.facebook.com/FADOC1721/posts/1581039465614653",
+    date: "22-05-2022",
+  },
+  {
+    place: "Popular Art Centre",
+    location: "Ramallah",
+    caption:
+      "The calculation of cooperatives has gone beyond that it is only a place for sale. People interact and ask about it and wait for it. This means that we are happy and always motivated to continue and with more strength, and people’s belief in the necessity of the success of youth cooperatives, their support and the purchase of a Palestinian product is the nature of the calculation in its economic and social dimension: You find people claiming and publishing for the Hesba! And people are calling to get them of all kinds on the side! And people are going through all the cooperatives to buy and support everyone! And there are other people and cooperatives, and if a particular product increases to another, it exhausts their worries and begins to think and work on how to help with this issue! We are waiting for you on Tuesday the 24th of the month, with new and fresh products and lower prices this time also, Watermelon, molokhia, peas, onions, potatoes, baladi eggs, ewes’ dairy, baladi garlic, eggplant, zucchini, zucchini, zucchini, and regular, ...., and many other things too, in short, the home ration that you buy all from Hisbah.",
+    images: [
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/283055802_10159262245582606_8315403027835176373_n.jpg?stp=dst-jpg_p960x960&_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=SvV_TRb-bjwAX9NM9nk&_nc_ht=scontent-bos5-1.xx&oh=00_AT_Yka9mZSYwDtGFXZsoT0VKUWFqKsEagLJOh1-WEBf_JA&oe=6299042A",
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/283112049_10159262253597606_271813085061389906_n.jpg?stp=dst-jpg_p720x720&_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=sxOakQInnE4AX9YRORr&_nc_ht=scontent-bos5-1.xx&oh=00_AT9yLfOVr0TYHJMbNAAoAx9Ykc2IyBu-p4CPCo7IIw8Hzw&oe=62992CED",
+    ],
+    url: "https://www.facebook.com/rami.massad.5/posts/10159262245632606",
+    date: "21-05-2022",
+  },
+  {
+    place: "Amal Cooperative",
+    location: "Madama",
+    caption:
+      'The "Amal Cooperative" (Hope Cooperative) was launched several months ago with efforts and a self-initiative in the village of Madama, south of Nablus. The cooperative includes 6 people (3 young men and 3 girls), who planted a land area of 5 dunams with wheat, and recently a plot of land was rehabilitated and planted with squash and zucchini seedlings. Here, we extend our thanks to the individuals and partner cooperatives who donated to support the cooperative in covering part of their needs in rehabilitating and cultivating the land. Hand in hand towards strengthening the youth collaborative work model',
+    images: [
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/278579251_10159220776617606_4102844932614828922_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=DpEvWIR9VfAAX98d8kH&_nc_oc=AQksNKmM3aVj8Dose1ybEnTiInHUqdMUJVEn665xQZybpZjX7F9bio7UWYVtpAByVN8&_nc_ht=scontent-bos5-1.xx&oh=00_AT_IEyR0i1dPOPSbV7Rl6hEO8BDEitfkMpO5Wf7OwYYUYw&oe=629A88A0",
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/279066442_10159220776757606_8056137088387694163_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=-7oOV69myiEAX9HvSr7&tn=0nVerItEGiwdW2Iv&_nc_ht=scontent-bos5-1.xx&oh=00_AT90R6YE7OiyKv9_2heshtZdaPjJYwxzOxqENHCJjmf3Zw&oe=629A618A",
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/279086768_10159220776952606_6820224447319035145_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=5HnK6msjY7wAX8C3Ljj&tn=0nVerItEGiwdW2Iv&_nc_ht=scontent-bos5-1.xx&oh=00_AT_CtKC90FkV9XG9ETIACblmIRLgctKbQEZ4F94gdpG-4Q&oe=62993CC7",
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/278913303_10159220777047606_7016788221030991112_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=la-e1_C9eJAAX9JdMye&tn=0nVerItEGiwdW2Iv&_nc_ht=scontent-bos5-1.xx&oh=00_AT8GNOHyyFs7abKi7z-t-sJjhHgopwFsKQ767_xCqSIvBA&oe=6299C713",
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/278605339_10159220777252606_7480923602681196753_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=8K29grmgP5AAX9Qrdeq&_nc_ht=scontent-bos5-1.xx&oh=00_AT9PcSYwOkdSf4UeBdA5yPLMmb6HY2CqWfAavKA83Nw2-w&oe=62995E91",
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/279398768_10159220798352606_1142764448477206330_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=MPq2gJ0B9tUAX-2Pg8o&_nc_oc=AQmBq_YPzOUkSa4QoAD_Ez5kgRFNVsXIBbExNIRPNWwIHl9G5MohNoW2YZq9BV8EVlU&_nc_ht=scontent-bos5-1.xx&oh=00_AT_sf6-pitwoDJt5laeoRbOgZzRuqIOhs4IhTah6HkYq-Q&oe=62990EAE",
+    ],
+    url: "https://www.facebook.com/rami.massad.5/posts/10159220777332606",
+    date: "21-05-2022",
+  },
+  {
+    place: "",
+    location: "Salfit",
+    caption:
+      "As part of our ongoing campaign, which is based on the Youth Partnership Forum and the AlAwda Center to help youth cooperatives and small farmers in marketing their products, 50 food baskets of seasonal products and vegetables were prepared and distributed to 50 families in need in Salfit, Nablus and Tulkarm during the holy month of Ramadan.",
+    images: [
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/278448915_1566148987103701_4363967582540297489_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=5uMWQ8CG0HcAX8-rP7D&_nc_ht=scontent-bos5-1.xx&oh=00_AT_-nOFUXt9uLjc8PCIORYT06HeaUD61apVEuwFx3vhe_w&oe=62991FDD",
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/277807109_1566148893770377_8853978002044905766_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=gzZhx3DnlZoAX9QL4WD&_nc_ht=scontent-bos5-1.xx&oh=00_AT9LVlkVpM7ITJFaO_IpGlnQiLdV31KG2s5Npq6noOLaBQ&oe=629A84BE",
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/278377475_1566149017103698_2749334675565381349_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WmXyDEFUh30AX_xREKG&_nc_ht=scontent-bos5-1.xx&oh=00_AT-0Yc7QbgLuhWa6cLx31Snz6SBODp0M0GEDHtKQnVwjYA&oe=629A4048",
+    ],
+    url: "https://www.facebook.com/rami.massad.5/posts/10159196615547606",
+    date: "16-04-2022",
+  },
+  {
+    place: "Bethlehem University",
+    location: "Bethlehem",
+    caption:
+      "The Department of Social Sciences/Master of Social Work program held a dialogue on youth cooperatives as a model of the resistance economy. The dialogue dealt with the experience of forming youth cooperatives in Palestine in terms of concept and structure, and its relationship to the social construction of a steadfast and resilient society.",
+    images: [
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/277553087_1555612514824015_8612088740341782512_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=gRW65CAEt9sAX8xc7EA&tn=0nVerItEGiwdW2Iv&_nc_ht=scontent-bos5-1.xx&oh=00_AT-VAxPMoCzlqaz0OehoOhPT_Bl5R3D12gg_Rc7mZ3-nzQ&oe=629AF47F",
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/277585218_1555612484824018_7647758926470458188_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=psepfWOgjLgAX_pd1TE&tn=0nVerItEGiwdW2Iv&_nc_ht=scontent-bos5-1.xx&oh=00_AT-_kyqLE0rKNslKTDwn2_BDziDRc5Zo_0tWLydlw4QrgA&oe=62996745",
+      "https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/277174539_1555612438157356_711497510498534381_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=GdVaAqXtIiEAX-iHYoP&tn=0nVerItEGiwdW2Iv&_nc_ht=scontent-bos5-1.xx&oh=00_AT8ruh2U1xpTmWrUY2jrTO42wXr3QbIWlc6dCEni9l1uxQ&oe=629ADC15",
+    ],
+    url: "https://www.facebook.com/FADOC1721/posts/1555612568157343",
+    date: "01-04-2022",
+  },
+];
+
+export default posts;
