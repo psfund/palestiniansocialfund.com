d4c53ab8643ad319ede92d6a94e1a6c2629a70c0
Adam Albarghouthi <adambargh@gmail.com>
2022-05-30 14:23:59 -0400

    import react-image-lightbox css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import { appWithTranslation } from "next-i18next";
 import "src/styles/globals.css";
+import "react-image-lightbox/style.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
