860a45c0dfa8d6d354513399d9b44962826d955c
Adam Albarghouthi <adambargh@gmail.com>
2023-03-09 14:25:00 -0500

    minor

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -73,7 +73,7 @@ export default function Navbar() {
                     Track fund
                   </a>
                   <a
-                    href="/pamphlet/introduction"
+                    href="/pamphlet"
                     className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                   >
                     Pamphlet
